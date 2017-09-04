0b188d368497005c09727c4f517634cab617da73
Thadeu Luz <thadeuluz@gmail.com>
2017-09-04 08:36:24 -0300

    Exercise

---
+++ python_exercises/Keras.py
@@ -0,0 +1,42 @@
+import numpy as np
+from keras.utils import np_utils
+import tensorflow as tf
+
+# Set random seed
+np.random.seed(42)
+
+# Our data
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]]).astype('float32')
+y = np.array([[0], [1], [1], [0]]).astype('float32')
+
+# Initial Setup for Keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+
+# Building the model
+xor = Sequential()
+
+# Add required layers
+xor.add(Dense(32, input_dim=X.shape[1]))
+xor.add(Activation('tanh'))
+
+xor.add(Dense(1))
+xor.add(Activation('sigmoid'))
+
+# Specify loss as "binary_crossentropy", optimizer as "adam",
+# and add the accuracy metric
+xor.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# Uncomment this line to print the model architecture
+xor.summary()
+
+# Fitting the model
+history = xor.fit(X, y, epochs=50, verbose=0)
+
+# Scoring the model
+score = xor.evaluate(X, y)
+print("\nAccuracy: ", score[-1])
+
+# Checking the predictions
+print("\nPredictions:")
+print(xor.predict_proba(X))
